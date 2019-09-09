f35b7afd819a8250bc6dda12df1d53e186f89870
Olly Hayes <olly.hayes@gmail.com>
2019-09-09 20:19:11 +0100

    Load (fake) data from API

---
+++ src/api.js
@@ -0,0 +1,24 @@
+export async function getWeatherForCity(city) {
+	// const result = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9753e913ea64baa961f8b3627b9b0cf3');
+
+	// const json = await result.json();
+
+	const json = {
+		'coord': { 'lon': -0.13, 'lat': 51.51 },
+		'weather': [
+			{ 'id': 300, 'main': 'Drizzle', 'description': 'light intensity drizzle', 'icon': '09d' }
+		],
+		'base': 'stations',
+		'main': { 'temp': 26, 'pressure': 1012, 'humidity': 81, 'temp_min': 279.15, 'temp_max': 281.15 },
+		'visibility': 10000, 'wind': { 'speed': 4.1, 'deg': 80 },
+		'clouds': { 'all': 90 },
+		'dt': 1485789600, 'sys': { 'type': 1, 'id': 5091, 'message': 0.0103, 'country': 'GB', 'sunrise': 1485762037, 'sunset': 1485794875 },
+		'id': 2643743, 'name': 'London', 'cod': 200
+	};
+
+	await new Promise(resolve => setTimeout(resolve, 1000));
+
+	return {
+		temperature: json.main.temp
+	};
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import Vue from 'vue';
+import {getWeatherForCity} from './api';
 
 const app = new Vue({
 	el: '#app',
 	data: {
-		dayIsGood: true
+		temperature: undefined
+	},
+	computed: {
+		dayIsGood() {
+			return this.temperature > 25;
+		}
+	},
+	methods: {
+		async getWeather() {
+			const weather = await getWeatherForCity('Londonn');
+
+			this.temperature = weather.temperature;
+		}
 	}
 });
+
+app.getWeather();
